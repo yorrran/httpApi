57abe9c7625d2a21ad053bd444553bfa4ae9d36e
Pang Yaran <PangYaran@hotmail.sg>
2018-07-02 17:07:03 +0800

    Fix bugs for timestamp and delete

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ clone the project
 | Get           | localhost:3000/products/EXISTING_KEY                                  | Records with matching EXISTING_KEY                    |
 | Get           | localhost:3000/products/EXISTING_KEY?timestamp = TIME_STAMP           | Records with matching EXISTING_KEY and TIME_STAMP     |
 | POST          | localhost:3000/products                                               | Records with specified {key: KEY_VALUE, value: Value} |
-| POST          | localhost:3000/products/EXISTING_KEY                                  | Records with specified key will be removed            |
+| POST          | localhost:3000/products/RECORD_ID                                     | Records with specified key will be removed            |
 
 ## Requirements
 node, npm
--- api/routes/products.js
+++ api/routes/products.js
@@ -14,6 +14,8 @@ router.get("/", (req, res, next) => {
   Product.find().exec()
     .then(docs => {
       console.log(docs);
+      console.log("the timestamp: ", docs[5].timestamp.getTime());
+      console.log("the timestamp: ", docs[6].timestamp.getTime());
       if (docs.length >= 0) {
         res.status(200).json(docs);
       } else {
@@ -42,7 +44,7 @@ router.post("/", (req, res, next) => {
       var dt = result.timestamp;
       var hourTime = dt.getHours();
       var minTime = dt.getMinutes();
-      var timeStamp = hourTime > 12 ? hourTime - 12 + ':' + minTime + 'PM' : hourTime + ':' + minTime + 'AM';
+      var timeStamp = hourTime > 12 ? hourTime - 12 + ':' + minTime + ' PM' : hourTime + ':' + minTime + ' AM';
       res.status(201).json({
         message: "Handling POST requests to /products",
         createdProduct: {
@@ -62,16 +64,36 @@ router.post("/", (req, res, next) => {
 
 router.get("/:key", (req, res, next) => {
   const keyValue = req.params.key;
+  var i;
+  var resValue = '';
   var datetimestamp = req.query.timestamp;
   if (datetimestamp) {
-    Product.find({ key: keyValue, timestamp: datetimestamp })
+    Product.find({ key: keyValue }).sort({ timestamp: -1 })
       .exec()
       .then(doc => {
-        console.log("From timestamp", doc);
-        if (doc) {
-          res.status(200).json({
-            value: doc[0].value
-          });
+        datetimestamp = parseInt(datetimestamp);
+        if (doc.length > 0) {
+          for (i = 0; i < doc.length; i++) {
+            if (doc[i].timestamp.getTime() === datetimestamp) {
+              resValue = doc[i].value;
+            }
+            else if (i !== doc.length - 1) {
+              if (doc[i].timestamp.getTime() > datetimestamp && doc[i + 1].timestamp.getTime() < datetimestamp) {
+                resValue = doc[i + 1].value;
+              }
+              else if (doc[i].timestamp.getTime() < datetimestamp) {
+                resValue = doc[i].value;
+              }
+            }
+          }
+          if (resValue) {
+            res.status(200).json({
+              value: doc[0].value
+            });
+          }
+          else {
+            res.status(404).json({ message: "No valid entry found for giving timestamp" });
+          }
         } else {
           res.status(404).json({ message: "No valid entry found for provided ID" });
         }
@@ -82,7 +104,7 @@ router.get("/:key", (req, res, next) => {
       });
   }
   else {
-    Product.find({ key: keyValue }).sort({ timestamp: -1 }).limit(1)
+    Product.find({ key: keyValue }).sort({ timestamp: -1 })
       .exec()
       .then(doc => {
         console.log("From database", doc);
@@ -101,9 +123,9 @@ router.get("/:key", (req, res, next) => {
   }
 });
 
-router.delete("/:key", (req, res, next) => {
-  const id = req.params.key;
-  Product.findOneAndRemove({ key: id })
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findByIdAndRemove({ _id: id })
     .exec()
     .then(result => {
       res.status(200).json(result);
